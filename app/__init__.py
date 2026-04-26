b58d9120692278fda6b7572e81e40f0b3ba5e7ff
Mateus Lopes <mateus_tiwu@hotmail.com>
2026-04-26 15:14:58 -0500

    adiciona product db model

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,10 @@
 from flask import Flask
-from .routes.main import main_bp
 from pymongo import MongoClient
 
 db = None
 
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(main_bp)
     app.config.from_object('config.Config')
     global db
 
@@ -15,4 +13,8 @@ def create_app():
         db = client.get_default_database()
     except Exception as e:
         print(f'Erro ao realizar a conexão com o banco de dados: {e}')
+
+    from .routes.main import main_bp
+    app.register_blueprint(main_bp)
+
     return app
--- app/routes/main.py
+++ app/routes/main.py
@@ -1,6 +1,10 @@
 from flask import Blueprint, jsonify, request
+
+from app.models.products import ProductDBModel
 from app.models.user import LoginPayload
 from pydantic import ValidationError
+from app import db
+from bson import ObjectId
 
 main_bp = Blueprint('main_bp', __name__)
 
@@ -20,13 +24,27 @@ def login():
     else:
         return jsonify({"message": "Acesso não autorizado"}), 403
 
-@main_bp.route('/products')
+@main_bp.route('/products', methods=['GET'])
 def get_products():
-    return jsonify({"message": "Esta é a rota de listagem dos produtos"})
+    products_cursor = db.products.find({})
+    products_list = [ProductDBModel(**product).model_dump(by_alias=True, exclude_none=True) for product in products_cursor]
 
-@main_bp.route('/products/<int:product_id>')
+    return jsonify(products_list)
+
+@main_bp.route('/products/<string:product_id>')
 def get_product_by_id(product_id):
-    return jsonify({"message": f"Esta é a rota de visualização detalhada do produto de id {product_id}"})
+    try:
+        oid = ObjectId(product_id)
+    except Exception as e:
+        return jsonify({"error": f"Erro ao transformar o {product_id} em ObjectId: {e}"}), 400
+
+    product = db.products.find_one({'_id': oid})
+
+    if product:
+        product_model = ProductDBModel(**product).model_dump(by_alias=True, exclude_none=True)
+        return jsonify(product_model)
+    else:
+        return jsonify({"error": f"Produto com o id {product_id} não encontrado"}), 404
 
 @main_bp.route('/products/<int:product_id>', methods=['PUT'])
 def update_product(product_id):
